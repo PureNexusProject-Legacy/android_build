7434ddf0984de9ec5a8d689322d174170884c46b
JustArchi <JustArchi@JustArchi.net>
2014-06-22 14:37:30 +0200

    Build: Add support for specifying build variant in brunch/breakfast
    This little modification allows specifying build variant in brunch/
    breakfast commands. For example we can use "brunch shamu user" to build
    user variant instead of default user.
    
    When no extra argument is given, userdebug is default.
    
    Change-Id: If4169cee368aadd52996c34846b62aeadc2b0ff3

--- envsetup.sh
+++ envsetup.sh
@@ -525,6 +525,7 @@ function brunch()
 function breakfast()
 {
     target=$1
+    local variant=$2
     CUSTOM_DEVICES_ONLY="true"
     unset LUNCH_MENU_CHOICES
     for f in `/bin/ls vendor/nexus/vendorsetup.sh 2> /dev/null`
@@ -544,7 +545,10 @@ function breakfast()
             lunch $target
         else
             # This is probably just the nexus model name
-            lunch nexus_$target-user
+            if [ -z "$variant" ]; then
+                variant="userdebug"
+            fi
+            lunch nexus_$target-$variant
         fi
     fi
     return $?
