9c3ee5f6a14aef9fd164cd3bcf867b97f311a33e
beanstown106 <nbenis106@gmail.com>
2015-12-26 19:00:12 -0500

    this is PureNexus

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -600,7 +600,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   #script.Print("Target: %s" % CalculateFingerprint(
   #    oem_props, oem_dict, OPTIONS.info_dict))
   script.Print("********************")
-  script.Print("**   Pure Stock   **")
+  script.Print("**   Pure NeXus   **")
   script.Print("********************")
 
   script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
